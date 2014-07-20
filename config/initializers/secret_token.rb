79ebe166ea297f119189ec0c6f3f5b835fa861da
Jahveed Shirzad <xjavdx@gmail.com>
2014-07-19 21:43:39 -0400

    Created a rails app and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Piknic::Application.config.secret_token = 'ddfd4380b522b3932ee90a3c8863928f45e558bd1479a0e19c109dab967e364f2638df44234dcfa93d987b070c505afd74e61220c45db63bb5e668db6b2eb7cf'
