2ab3b1c93c1e698caaff7bac8868a22cc05182ef
Jahveed Shirzad <xjavdx@gmail.com>
2014-07-24 19:20:41 -0400

    Add status validations and tests

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,4 +1,9 @@
 class Status < ActiveRecord::Base
   attr_accessible :contenet, :user_id
   belongs_to :user
+
+  validates :contenet, presence: true,
+                      length: { minimum: 2 }
+
+  validates :user_id, presence: true
 end
--- test/unit/status_test.rb
+++ test/unit/status_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class StatusTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "that a status requires contenet" do
+    status = Status.new
+    assert !status.save
+    assert !status.errors[:contenet].empty?
+  end
+
+  test "that a status's contenet is at least 2 letters" do
+    status = Status.new
+    status.contenet = "H"
+    assert !status.save
+    assert !status.errors[:contenet].empty?
+  end 
+
+  test "that a status has a user id" do
+    status = Status.new
+    status.contenet = "Hello"
+
+    assert !status.save
+    assert !status.errors[:user_id].empty?
+  end
 end
